6bcc0f89b5368faaa72db6e05cdb584783838443
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-01-13 12:09:09 -0300

    commit init

---
+++ src/main/kotlin/br/com/matheusfinamor/rest_with_spring_boot_and_kotlin_matheusfinamor/RestWithSpringBootAndKotlinMatheusfinamorApplication.kt
@@ -0,0 +1,11 @@
+package br.com.matheusfinamor.rest_with_spring_boot_and_kotlin_matheusfinamor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinMatheusfinamorApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinMatheusfinamorApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/matheusfinamor/rest_with_spring_boot_and_kotlin_matheusfinamor/RestWithSpringBootAndKotlinMatheusfinamorApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.matheusfinamor.rest_with_spring_boot_and_kotlin_matheusfinamor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestWithSpringBootAndKotlinMatheusfinamorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
