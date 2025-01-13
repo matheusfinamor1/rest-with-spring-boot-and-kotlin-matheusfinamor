d4c16d5482d67a4e560fe974d9a972d93705fec1
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-01-13 12:21:59 -0300

    commit base

--- src/main/kotlin/br/com/matheusfinamor/rest_with_spring_boot_and_kotlin_matheusfinamor/Startup.kt
+++ src/main/kotlin/br/com/matheusfinamor/rest_with_spring_boot_and_kotlin_matheusfinamor/Startup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RestWithSpringBootAndKotlinMatheusfinamorApplication
+class Startup
 
 fun main(args: Array<String>) {
-	runApplication<RestWithSpringBootAndKotlinMatheusfinamorApplication>(*args)
+	runApplication<Startup>(*args)
 }
