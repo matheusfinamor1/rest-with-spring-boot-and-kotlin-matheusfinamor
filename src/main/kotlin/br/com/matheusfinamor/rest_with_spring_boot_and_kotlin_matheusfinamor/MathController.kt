63247bf7680797cc9ccd6124dc8b099b34771c9f
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-01-13 21:15:44 -0300

    sum without handling exception

---
+++ src/main/kotlin/br/com/matheusfinamor/rest_with_spring_boot_and_kotlin_matheusfinamor/MathController.kt
@@ -0,0 +1,34 @@
+package br.com.matheusfinamor.rest_with_spring_boot_and_kotlin_matheusfinamor
+
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class MathController {
+
+    val counter: AtomicLong = AtomicLong()
+
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+            /** @PathVariable recupera dados da URL */
+    fun sum(
+        @PathVariable(value = "numberOne") numberOne: String?,
+        @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        return convertToDouble(numberOne) + convertToDouble(numberTwo)
+    }
+
+    private fun convertToDouble(strNumber: String?): Double {
+        if (strNumber.isNullOrBlank()) return 0.0
+        val number = strNumber.replace(",".toRegex(), ".")
+        return if (isNumeric(number)) number.toDouble() else 0.0
+    }
+
+    private fun isNumeric(strNumber: String?): Boolean {
+        if (strNumber.isNullOrBlank()) return false
+        val number = strNumber.replace(",".toRegex(), ".")
+        return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
+    }
+}
