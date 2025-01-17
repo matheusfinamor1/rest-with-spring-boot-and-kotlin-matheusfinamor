bae7a899037c3fcb0cf12a8d1bb24008f384c573
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-01-17 11:47:06 -0300

    custom JSON Serialization (order attributes, handle name, ignored attribute JSON...)

--- src/main/kotlin/br/com/matheusfinamor/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/matheusfinamor/data/vo/v1/PersonVO.kt
@@ -1,8 +1,16 @@
 package br.com.matheusfinamor.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+/** coloca os dados do objeto JSON na ordem desejada */
+@JsonPropertyOrder("id","address","first_name", "last_name", "gender")
 data class PersonVO(
     var id: Long = 0,
+    /** altera o nome do atributo do objeto JSON */
+    @field:JsonProperty("first_name")
     var firstName: String = "",
+    @field:JsonProperty("last_name")
     var lastName: String = "",
     var address: String = "",
     var gender: String = ""
