f0d676d8f3744718ee23cb2950a800cf878bda19
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-01-24 18:45:06 -0300

    Testing launch exceptions in the functions create and update

--- src/test/kotlin/br/com/matheusfinamor/mockito/services/PersonServiceTest.kt
+++ src/test/kotlin/br/com/matheusfinamor/mockito/services/PersonServiceTest.kt
@@ -1,14 +1,12 @@
 package br.com.matheusfinamor.mockito.services
 
+import br.com.matheusfinamor.mapper.custom.PersonMapper
 import br.com.matheusfinamor.mocks.MockPerson
 import br.com.matheusfinamor.repository.PersonRepository
 import br.com.matheusfinamor.services.PersonService
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
@@ -16,7 +14,6 @@ import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
 import java.util.*
-import kotlin.test.assertFalse
 
 @ExtendWith(MockitoExtension::class)
 class PersonServiceTest {
@@ -28,6 +25,9 @@ class PersonServiceTest {
 
     @Mock
     private lateinit var repository: PersonRepository
+
+    @Mock
+    private lateinit var mapper: PersonMapper
 
     @BeforeEach
     fun setUp() {
@@ -59,17 +59,53 @@ class PersonServiceTest {
 
     @Test
     fun create() {
-    }
+        val entity = inputObject.mockEntity(1)
 
-    @Test
-    fun createV2() {
+        val persisted = entity.copy()
+        persisted.id = 1
+
+        `when`(repository.save(entity)).thenReturn(persisted)
+
+        val vo = inputObject.mockVO(1)
+        val result = service.create(vo)
+
+        assertNotNull(result)
+        assertNotNull(result.key)
+        assertNotNull(result.links)
+        assertTrue(result.links.toString().contains("</person/1>;rel=\"self\""))
+        assertEquals("Address Test1", result.address)
+        assertEquals("First Name Test1", result.firstName)
+        assertEquals("Last Name Test1", result.lastName)
+        assertEquals("Female", result.gender)
     }
 
     @Test
     fun update() {
+        val entity = inputObject.mockEntity(1)
+
+        val persisted = entity.copy()
+        persisted.id = 1
+
+        `when`(repository.findById(1)).thenReturn(Optional.of(entity))
+        `when`(repository.save(entity)).thenReturn(persisted)
+
+        val vo = inputObject.mockVO(1)
+        val result = service.update(vo)
+
+        assertNotNull(result)
+        assertNotNull(result.key)
+        assertNotNull(result.links)
+        assertTrue(result.links.toString().contains("</person/1>;rel=\"self\""))
+        assertEquals("Address Test1", result.address)
+        assertEquals("First Name Test1", result.firstName)
+        assertEquals("Last Name Test1", result.lastName)
+        assertEquals("Female", result.gender)
     }
 
     @Test
     fun delete() {
+        val entity = inputObject.mockEntity(1)
+        `when`(repository.findById(1)).thenReturn(Optional.of(entity))
+        service.delete(1)
     }
 }
