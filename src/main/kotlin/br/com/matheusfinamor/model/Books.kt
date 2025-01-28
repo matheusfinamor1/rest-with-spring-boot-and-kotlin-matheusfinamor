0bc9e0f0dc3608e533a1d78750e39c4e82cd1615
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-01-28 14:13:06 -0300

    Create books entity

---
+++ src/main/kotlin/br/com/matheusfinamor/model/Books.kt
@@ -0,0 +1,21 @@
+package br.com.matheusfinamor.model
+
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.util.Date
+
+@Entity
+@Table(name = "books")
+data class Books(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0,
+    @Column(length = 50)
+    var author: String = "",
+    @Column(name = "launch_date", nullable = true)
+    var launchDate: Date? = null,
+    @Column
+    var price: BigDecimal = BigDecimal.ZERO,
+    @Column(length = 80)
+    var title: String = ""
+)
