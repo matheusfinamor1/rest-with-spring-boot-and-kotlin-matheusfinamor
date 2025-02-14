6edd5e1648d357293bf7f374ffd3225a3d290bcb
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-02-14 16:47:03 -0300

    ability CORS global form

--- src/main/kotlin/br/com/matheusfinamor/config/WebConfig.kt
+++ src/main/kotlin/br/com/matheusfinamor/config/WebConfig.kt
@@ -1,12 +1,17 @@
 package br.com.matheusfinamor.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
+    @Value("\${cors.originPatterns:default}")
+    private val corsOriginPatterns: String = ""
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
         configurer.favorParameter(false)
@@ -15,5 +20,13 @@ class WebConfig : WebMvcConfigurer {
             .defaultContentType(MediaType.APPLICATION_JSON)
             .mediaType("json", MediaType.APPLICATION_JSON)
             .mediaType("xml", MediaType.APPLICATION_XML)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins(*allowedOrigins)
+            .allowCredentials(true)
     }
 }
